8b60c859291f5ffeea4ef1b1de9919e4e18e612b
Gabriel de Souza <51684325+GabrielGSD@users.noreply.github.com>
2023-06-14 02:14:17 +0000

    Feat: create course

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,20 +2,6 @@
 
 package model
 
-type Category struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description *string   `json:"description,omitempty"`
-	Products    []*Course `json:"products"`
-}
-
-type Course struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description *string   `json:"description,omitempty"`
-	Category    *Category `json:"category"`
-}
-
 type NewCategory struct {
 	Name        string  `json:"name"`
 	Description *string `json:"description,omitempty"`
